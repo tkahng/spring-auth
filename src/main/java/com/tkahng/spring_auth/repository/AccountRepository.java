40b9ff933e01d9b16ebadca842877dc638ed3b74
tkahng <tkahng@gmail.com>
2025-08-08 18:57:52 -0700

    update repository

--- src/main/java/com/tkahng/spring_auth/repository/AccountRepository.java
+++ src/main/java/com/tkahng/spring_auth/repository/AccountRepository.java
@@ -2,12 +2,12 @@ package com.tkahng.spring_auth.repository;
 
 
 import com.tkahng.spring_auth.domain.Account;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends CrudRepository<Account, String> {
+public interface AccountRepository extends JpaRepository<Account, String> {
     Optional<Account> findByUserIdAndProviderId(String userId, String providerId);
 }
--- src/main/java/com/tkahng/spring_auth/repository/UserRepository.java
+++ src/main/java/com/tkahng/spring_auth/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.tkahng.spring_auth.repository;
 
 import com.tkahng.spring_auth.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 }
